5a36c2aa4b746e46313771f8f32b9707a22b671d
superdyzio <perdekdawid@gmail.com>
2025-09-13 13:14:08 +0100

    make form example components lazy loaded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
-import { TemplateComponent } from './pages/template/template.component';
-import { ReactiveComponent } from './pages/reactive/reactive.component';
-import { SignalComponent } from './pages/signal/signal.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
-  { path: 'template', component: TemplateComponent },
-  { path: 'reactive', component: ReactiveComponent },
-  { path: 'signal', component: SignalComponent },
+  { 
+    path: 'template', 
+    loadComponent: () => import('./pages/template/template.component').then(m => m.TemplateComponent)
+  },
+  { 
+    path: 'reactive', 
+    loadComponent: () => import('./pages/reactive/reactive.component').then(m => m.ReactiveComponent)
+  },
+  { 
+    path: 'signal', 
+    loadComponent: () => import('./pages/signal/signal.component').then(m => m.SignalComponent)
+  },
   { path: '**', redirectTo: '' }
 ];
