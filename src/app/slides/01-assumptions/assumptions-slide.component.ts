cc2d56bbb30d5eb06d04ce7dae5376fa5486f711
superdyzio <perdekdawid@gmail.com>
2025-09-16 22:26:12 +0200

    slide1: assumptions

---
+++ src/app/slides/01-assumptions/assumptions-slide.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'afs-assumptions-slide',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './assumptions-slide.component.html',
+  styleUrl: './assumptions-slide.component.scss'
+})
+export class AssumptionsSlideComponent {}
