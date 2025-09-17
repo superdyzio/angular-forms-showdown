227ddafd9b9234a9e05dfa980309c220d08f5781
superdyzio <perdekdawid@gmail.com>
2025-09-17 18:04:08 +0200

    slide4: reactive forms

---
+++ src/app/slides/04-reactive-forms/reactive-forms-slide.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'afs-reactive-forms-slide',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reactive-forms-slide.component.html',
+  styleUrl: './reactive-forms-slide.component.scss'
+})
+export class ReactiveFormsSlideComponent {}
--- src/app/slides/slides.routes.ts
+++ src/app/slides/slides.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { AssumptionsSlideComponent } from './01-assumptions/assumptions-slide.component';
 import { UseCaseSlideComponent } from './02-use-case/use-case-slide.component';
 import { TemplateFormsSlideComponent } from './03-template-forms/template-forms-slide.component';
-import { Slide3Component } from './slide3/slide3.component';
+import { ReactiveFormsSlideComponent } from './04-reactive-forms/reactive-forms-slide.component';
 
 export const slidesRoutes: Routes = [
   { path: '', redirectTo: '1', pathMatch: 'full' },
   { path: '1', component: AssumptionsSlideComponent },
   { path: '2', component: UseCaseSlideComponent },
   { path: '3', component: TemplateFormsSlideComponent },
-  { path: '4', component: Slide3Component },
+  { path: '4', component: ReactiveFormsSlideComponent },
   { path: '**', redirectTo: '1' }
 ];
