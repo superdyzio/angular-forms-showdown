df2129f18702f2547acce87492185fc6142cef56
superdyzio <perdekdawid@gmail.com>
2025-09-17 17:52:48 +0200

    slide3: template forms

---
+++ src/app/slides/03-template-forms/template-forms-slide.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'afs-template-forms-slide',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template-forms-slide.component.html',
+  styleUrl: './template-forms-slide.component.scss'
+})
+export class TemplateFormsSlideComponent {}
--- src/app/slides/slides.routes.ts
+++ src/app/slides/slides.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { AssumptionsSlideComponent } from './01-assumptions/assumptions-slide.component';
 import { UseCaseSlideComponent } from './02-use-case/use-case-slide.component';
-import { Slide2Component } from './slide2/slide2.component';
+import { TemplateFormsSlideComponent } from './03-template-forms/template-forms-slide.component';
 import { Slide3Component } from './slide3/slide3.component';
 
 export const slidesRoutes: Routes = [
   { path: '', redirectTo: '1', pathMatch: 'full' },
   { path: '1', component: AssumptionsSlideComponent },
   { path: '2', component: UseCaseSlideComponent },
-  { path: '3', component: Slide2Component },
+  { path: '3', component: TemplateFormsSlideComponent },
   { path: '4', component: Slide3Component },
   { path: '**', redirectTo: '1' }
 ];
